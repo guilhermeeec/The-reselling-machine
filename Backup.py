d32cc92a083b79178fdc26ba354a17e9619a252c
guilhermeeec <guilhermepic2016@gmail.com>
2019-11-28 12:25:15 -0300

    Começando o código

---
+++ Backup.py
@@ -0,0 +1,10 @@
+import os
+
+def backup():
+
+    os.system('cls' or 'clear')
+
+    print('************************* BACKUPS ************************\N')
+
+    print('Lista de produtos com a data da última modificação')
+
