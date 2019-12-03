595a32c958118ef71bb943d9bc88417b6f4f8112
guilhermeeec <guilhermepic2016@gmail.com>
2019-12-03 11:59:15 -0300

    Backup consertado

--- Backup.py
+++ Backup.py
@@ -1,30 +1,27 @@
 import os
-
 def backup(quant_prod_exibidos=3):
-
     os.system('clear' or 'cls')
-
-    print('************************* BACKUPS ************************\n')
-
-    print('Lista de produtos com a data da última modificação')
     
+    print('************************* BACKUPS ************************\n')
+    
+    print('Lista de produtos com a data da última modificação')
+                    
     backups = open('Backup.txt', 'r')
     backups.seek(0)
-
+    
     matriz=[]
     
     if backups.read() == None:
         input('Não há produtos no backup')
         return
-    
+
     backups.seek(0)
     for produto in backups.readlines():
         matriz.append(produto.split('~')[:-1])
     
     backups.close()
-
-    n = min(len(matriz), quant_prod_exibidos)
     
+    n = min(len(matriz), quant_prod_exibidos)
 
     for produto in range(quant_prod_exibidos-3, n):
         print(matriz[produto][-1])
@@ -40,11 +37,59 @@ def backup(quant_prod_exibidos=3):
 
     if n < len(matriz):
         print('Pressione "m" para exibir mais')
+
     print('Pressione "D" para deletar um produto de Bakcup')
     print('Digite "v" para voltar')
-    
+
     entrada = input()
 
     if entrada == 'v' or entrada == 'V':
         return
 
+    elif (entrada == 'm' or entrada == 'M') and n < len(matriz):
+        backup(quant_prod_exibidos+3)
+        return
+
+    elif entrada == 'd' or entrada == 'D':
+        linha = input('Digite o número do produto a ser deletado: ')
+        
+        erro_caracter = False
+        
+        for caracter in linha:
+            if ord(caracter)<ord('0') or ord(caracter)>ord('9'):
+                erro_caracter = True
+        
+        if erro_caracter == True:
+            input('Comando inválido')
+            backup(quant_prod_exibidos)
+            return
+
+        else:
+            linha = int(linha)
+            if linha >= len(matriz):
+                input('Esse número não corresponde a nenhum produto')
+                backup(quant_prod_exibidos)
+                return
+            else:
+                backups = open('Backup.txt', 'r')
+                backups.seek(0)
+                produtos = backups.readlines()
+                produtos.pop(linha-1)
+                backups.close()
+                backups = open('Backup.txt', 'w')
+                backups.seek(0)
+                backups.writelines(produtos)
+                backups.close()
+                backup(quant_prod_exibidos)
+                return
+    
+    else:
+        input('Opção inválida')
+        backup(quant_prod_exibidos)
+        return
+
+def main():
+    backup()
+
+if __name__ == '__main__':
+    main()
